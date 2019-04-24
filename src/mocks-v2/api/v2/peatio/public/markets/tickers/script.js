4c7d3f2a4c331921cbb7947a42c6bc422ff7fab6
oyershov <oyershov@heliostech.fr>
2019-04-24 16:58:24 +0300

    Feature: add some local upgrades

--- src/mocks-v2/api/v2/peatio/public/markets/tickers/script.js
+++ src/mocks-v2/api/v2/peatio/public/markets/tickers/script.js
@@ -9,7 +9,20 @@ module.exports = {
             "low": "1150.0",
             "high": "1350.0",
             "last": "1250.0",
-            "vol": "120.0"
+            "vol": "120.0",
+            "price_change_percent": "+2.34%"
+        }
+    },
+    "btcusd": {
+        "at": ts,
+        "ticker": {
+            "buy": "1200.0",
+            "sell": "1300.0",
+            "low": "1150.0",
+            "high": "1350.0",
+            "last": "1250.0",
+            "vol": "120.0",
+            "price_change_percent": "+2.34%"
         }
     },
     "bchbtc": {
@@ -20,7 +33,8 @@ module.exports = {
             "low": "115.0",
             "high": "135.0",
             "last": "125.0",
-            "vol": "12.0"
+            "vol": "12.0",
+            "price_change_percent": "-1.55%"
         }
     },
     "ethbtc": {
@@ -31,7 +45,8 @@ module.exports = {
             "low": "0.238",
             "high": "0.253",
             "last": "0.245",
-            "vol": "200.0"
+            "vol": "200.0",
+            "price_change_percent": "+3.13%"
         }
     },
     "dashbtc": {
@@ -42,7 +57,8 @@ module.exports = {
             "low": "1.15",
             "high": "1.35",
             "last": "1.250",
-            "vol": "50.0"
+            "vol": "50.0",
+            "price_change_percent": "+5.00%"
         }
     },
     "ltcbtc": {
@@ -53,7 +69,8 @@ module.exports = {
             "low": "99.0",
             "high": "150.0",
             "last": "105.0",
-            "vol": "10.0"
+            "vol": "10.0",
+            "price_change_percent": "+0.00%"
         }
     },
     "xrpbtc": {
@@ -64,7 +81,8 @@ module.exports = {
             "low": "150.0",
             "high": "350.0",
             "last": "250.0",
-            "vol": "20.0"
+            "vol": "20.0",
+            "price_change_percent": "-1.00%"
         }
     },
     "xrpzar": {
@@ -86,7 +104,8 @@ module.exports = {
             "low": "150.0",
             "high": "350.0",
             "last": "250.0",
-            "vol": "20.0"
+            "vol": "20.0",
+            "price_change_percent": "+4.44%"
         }
     },
     "eurusd": {
@@ -97,7 +116,8 @@ module.exports = {
             "low": "150.0",
             "high": "350.0",
             "last": "250.0",
-            "vol": "20.0"
+            "vol": "20.0",
+            "price_change_percent": "-0.02%"
         }
     },
     "ethzar": {
@@ -108,7 +128,8 @@ module.exports = {
             "low": "150.0",
             "high": "350.0",
             "last": "250.0",
-            "vol": "20.0"
+            "vol": "20.0",
+            "price_change_percent": "+0.00%"
         }
     },
     "dashzar": {
@@ -119,7 +140,8 @@ module.exports = {
             "low": "150.0",
             "high": "350.0",
             "last": "250.0",
-            "vol": "20.0"
+            "vol": "20.0",
+            "price_change_percent": "+0.01%"
         }
     },
     "btczar": {
@@ -130,7 +152,8 @@ module.exports = {
             "low": "150.0",
             "high": "350.0",
             "last": "250.0",
-            "vol": "20.0"
+            "vol": "20.0",
+            "price_change_percent": "-0.19%"
         }
     }
 }
--- src/slanger.js
+++ src/slanger.js
@@ -24,7 +24,7 @@ const tradesMock = (ws, channel) => {
         [
           {
             "tid": tradeId,
-            "type": tradeSide,
+            "taker_type": tradeSide,
             "date": Date.now(),
             "price": price.toString(),
             "amount": amount.toString(),
